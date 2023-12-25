922d51ce1e9b3e5f4073974fcc7fb62197b9efde
mustafakhan345 <154807334+mustafakhan345@users.noreply.github.com>
2023-12-25 04:24:46 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numone",
+        message: "kindly inter your first number",
+    },
+    { type: "number",
+        name: "numtwo",
+        message: "kindly inter your second number",
+    },
+    {
+        type: "list",
+        name: "opperator",
+        choices: ['addition', 'subtraction', 'multiplication', "division"],
+        message: "Sellect your opperator:"
+    }
+]);
+if (answers.opperator === "addition") {
+    console.log(answers.numone + answers.numtwo);
+}
+if (answers.opperator === "subtraction") {
+    console.log(answers.numone - answers.numtwo);
+}
+if (answers.opperator === "multiplication") {
+    console.log(answers.numone * answers.numtwo);
+}
+if (answers.opperator === "division") {
+    console.log(answers.numone / answers.numtwo);
+}
